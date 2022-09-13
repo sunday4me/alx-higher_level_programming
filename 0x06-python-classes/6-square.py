76b3a44ef17f3a7f033bc3af1d67f99e2d70bb21
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2022-09-13 21:47:35 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,4 +59,4 @@ class Square:
                 or (value[0] < 0) or (value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = 
+            self.__position = value
